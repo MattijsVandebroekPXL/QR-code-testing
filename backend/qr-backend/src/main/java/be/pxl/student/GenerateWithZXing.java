33ef9a93a179e6a7aad0203d8c8d59a5609ded1f
MattijsVandebroekPXL <mattijs.vandebroek@student.pxl.be>
2026-03-12 09:58:37 +0100

    tried to use backend stuff

---
+++ backend/qr-backend/src/main/java/be/pxl/student/GenerateWithZXing.java
@@ -0,0 +1,16 @@
+package be.pxl.student;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class GenerateWithZXing
+{
+    public static void QRthing()
+    {
+        String qrText = "Dit is een test.";
+        String filePath = "zxingQrcode.png";
+        int width = 300;
+        int height = 300;
+        
+    }
+}
